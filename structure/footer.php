4a19b61f57fecdc74da87111ae9646b924fcb18c
Daniel Gros <danielgrosbgns@gmail.com>
2023-11-10 10:44:29 +0100

    head.php header.php footer.php modified with basic syntaxes

---
+++ structure/footer.php
@@ -0,0 +1,6 @@
+<footer>
+  <p>Projet Etudiant de <a href="mailto:danielgrosbngs@gmail.com">Daniel Gros</a></p>
+</footer>
+</body>
+
+</html>
---
+++ structure/head.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html lang="fr">
+
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <meta name="description" content="blog de sheetcheat personnel pour étudiant en développement web">
+  <title>Sheetcheat</title>
+</head>
+
+<body>
